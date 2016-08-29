6780f5675b45ed8678d8582b70b7bdbab46ab556
apsaxena24 <apsaxena24@gmail.com>
2016-08-29 11:57:43 +0100

    fixed product counting bug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,5 +3,8 @@ class ProductsController < ApplicationController
     session[:cart] ||= []
     @cart_size = session[:cart].count
     @products = Product.all
+    @products_in_cart = session[:cart].map {|id| Product.find id}
+    @product_map = Hash.new(0)
+    @products_in_cart.map{|product| @product_map[product]+=1}
   end
 end
